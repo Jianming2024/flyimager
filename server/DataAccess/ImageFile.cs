dd84c601c3de1ffaa1bec3603f4a10d9fa0d445c
Jianming Fan <151937346+Jianming2024@users.noreply.github.com>
2025-11-07 23:37:32 +0100

    first commit

---
+++ server/DataAccess/ImageFile.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess;
+
+public class ImageSharing
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string Id { get; set; } = null!;
+    public string UploaderUserId { get; set; } = null!;
+    public string ObjectKey { get; set; } = null!; // S3/Tigris key
+    public string FileName { get; set; } = null!;
+    public string ContentType { get; set; } = null!;
+    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+}
